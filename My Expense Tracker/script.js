f53aac7c0875b8d80ac5b1c215cef986eecfad6a
Brandon Hill <brandonhill018@gmail.com>
2022-01-26 23:28:32 -0700

    start js

---
+++ My Expense Tracker/script.js
@@ -0,0 +1,7 @@
+const incomeTotal = document.getElementsByClassName('income-total');
+const expensesTotal = document.getElementsByClassName('expenses-total');
+const addIncomeForm = document.getElementsByClassName('income-form');
+const addExpenseForm = document.getElementsByClassName('form-container');
+const balance = document.getElementsByClassName('balance');
+const addIncomeBtn = document.getElementsByClassName('income-button');
+const addExpenseBtn = document.getElementsByClassName('expenses-button');
